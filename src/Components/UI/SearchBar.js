9d4c7b2cc97377a811672924a03f1e1345ccaf32
MarcinMaster <m.luba505@gmail.com>
2022-05-26 06:57:16 +0200

    Added Header, Navbar, SearchBar and coresponding CSS files

---
+++ src/Components/UI/SearchBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './SearchBar.css'
+
+const SearchBar = () => {
+  return (
+      <div className='form'>
+    <form>
+      <div className="input">
+        <input />
+      </div>
+      <div className="form-button">
+        <button className='button'>FETCH FORECAST</button>
+      </div>
+    </form>
+    </div>
+  );
+};
+
+export default SearchBar;
