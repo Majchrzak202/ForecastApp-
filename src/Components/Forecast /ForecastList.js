3a5e92494c4bcf3a01da9a5bb0a8440c9bbccc37
MarcinMaster <m.luba505@gmail.com>
2022-05-31 06:08:43 +0200

    Added a map method to dynamicly render ForecastBlock components

--- src/Components/Forecast /ForecastList.js
+++ src/Components/Forecast /ForecastList.js
@@ -4,58 +4,34 @@ import "./ForecastList.css";
 import moment from "moment";
 
 const ForecastList = ({ forecast, weather }) => {
+  const blocks = [];
+
+  if (forecast === null) {
+    return null;
+  } else {
+    for (let i = 0; i < 5; i++) {
+      blocks.push(
+        <ForecastBlock
+          min_temp={forecast.daily[i].temp.min}
+          max_temp={forecast.daily[i].temp.max}
+          icon_id={forecast.daily[i].weather[0].icon}
+          descript={forecast.daily[i].weather[0].main}
+          date={moment().add(i, "d").toDate()}
+        />
+      );
+    }
+  }
+
   return (
     <div>
-      <div className='title'>
-        <h2>{weather && weather.name}, {weather && weather.sys.country}</h2>
+      <div className="title">
+        <h2>
+          {weather && weather.name}, {weather && weather.sys.country}
+        </h2>
       </div>
-    <ul className="list">
-      {forecast && (
-        <ForecastBlock
-          min_temp={forecast.daily[0].temp.min}
-          max_temp={forecast.daily[0].temp.max}
-          icon_id={forecast.daily[0].weather[0].icon}
-          descript={forecast.daily[0].weather[0].main}
-          date={moment().add(0, "d").toDate()}
-        />
-      )}
-      {forecast && (
-        <ForecastBlock
-          min_temp={forecast.daily[1].temp.min}
-          max_temp={forecast.daily[1].temp.max}
-          icon_id={forecast.daily[1].weather[0].icon}
-          descript={forecast.daily[1].weather[0].main}
-          date={moment().add(1, "d").toDate()}
-        />
-      )}
-      {forecast && (
-        <ForecastBlock
-          min_temp={forecast.daily[2].temp.min}
-          max_temp={forecast.daily[2].temp.max}
-          icon_id={forecast.daily[2].weather[0].icon}
-          descript={forecast.daily[2].weather[0].main}
-          date={moment().add(2, "d").toDate()}
-        />
-      )}
-      {forecast && (
-        <ForecastBlock
-          min_temp={forecast.daily[3].temp.min}
-          max_temp={forecast.daily[3].temp.max}
-          icon_id={forecast.daily[3].weather[0].icon}
-          descript={forecast.daily[3].weather[0].main}
-          date={moment().add(3, "d").toDate()}
-        />
-      )}
-      {forecast && (
-        <ForecastBlock
-          min_temp={forecast.daily[4].temp.min}
-          max_temp={forecast.daily[4].temp.max}
-          icon_id={forecast.daily[4].weather[0].icon}
-          descript={forecast.daily[4].weather[0].main}
-          date={moment().add(4, "d").toDate()}
-        />
-      )}
-    </ul>
+      <ul className="list">
+        {forecast && blocks.map((item, index) => <div key={index}>{item}</div>)}
+      </ul>
     </div>
   );
 };
