6a412f35ff2eb01c27a0bc5174c83683634429fb
MarcinMaster <m.luba505@gmail.com>
2022-07-09 07:50:51 +0200

    Added a lazy load import for Routes / Removed Browser Router / Removed some production console.logs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
+
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/UI/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from "./Components/Pages/About";
-import Contact from "./Components/Pages/Contact";
-import PageNotFound from "./Components/Pages/PageNotFound";
 import Home from "./Components/Pages/Home";
-import ThankYou from "./Components/Pages/ThankYou";
-import useFetch from "./hooks/useFetch";
 import Footer from "./Components/UI/Footer";
+
+import useFetch from "./hooks/useFetch";
+
+const About = lazy(() => import("./Components/Pages/About"));
+const Contact = lazy(() => import("./Components/Pages/Contact"));
+const PageNotFound = lazy(() => import("./Components/Pages/PageNotFound"));
+const ThankYou = lazy(() => import("./Components/Pages/ThankYou"));
 
 const api = {
   base: "https://api.openweathermap.org/data/2.5/",
@@ -22,7 +25,6 @@ function App() {
   const GetWeather = async (data) => {
     let cityLat = data.coord.lat;
     let cityLon = data.coord.lon;
-    console.log(data)
     setWeather(data);
 
     const forecast = await fetch(
@@ -30,46 +32,38 @@ function App() {
     );
     const forecastData = await forecast.json();
     setForecast(forecastData);
-  }
+  };
 
-  const {sendRequest: fetchForecastHandler} = useFetch({url: `${api.base}weather?q=${search}&units=metric&APPID=${api.key}`}, GetWeather)
-
- /*  const fetchForecastHandler = async () => {
-    const response = await fetch(
-      `${api.base}weather?q=${search}&units=metric&APPID=${api.key}`
-    );
-    const data = await response.json();
-    let cityLat = data.coord.lat;
-    let cityLon = data.coord.lon;
-    setWeather(data);
-    console.log(data);
-  }; */
+  const { sendRequest: fetchForecastHandler } = useFetch(
+    { url: `${api.base}weather?q=${search}&units=metric&APPID=${api.key}` },
+    GetWeather
+  );
 
   return (
-    <Router>
-      <div>
-        <Navbar />
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              search={search}
-              setSearch={setSearch}
-              fetchForecastHandler={fetchForecastHandler}
-              forecast={forecast}
-              weather={weather}
-            />
-          }
-        ></Route>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/thank" element={<ThankYou />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-      <Footer/>
-    </Router>
+    <>
+      <Navbar />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                search={search}
+                setSearch={setSearch}
+                fetchForecastHandler={fetchForecastHandler}
+                forecast={forecast}
+                weather={weather}
+              />
+            }
+          ></Route>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/thank" element={<ThankYou />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
+      <Footer />
+    </>
   );
 }
 
