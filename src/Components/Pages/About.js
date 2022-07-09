8b54bf326686fd84d0506658edb473870a4f5045
MarcinMaster <m.luba505@gmail.com>
2022-07-09 07:51:42 +0200

    Added React.memo()

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,13 +1,16 @@
 import React from "react";
-import './About.css'
+import "./About.css";
 
 const About = () => {
-    return (
-        <div className='about'>
-            <h2> This is awesome Forecast App</h2>
-            <p>Just type-o desired City Name and see Weather Forecast for upcoming 5 days! </p>
-        </div>
-    )
-}
+  return (
+    <div className="about">
+      <h2> This is awesome Forecast App</h2>
+      <p>
+        Just type-o desired City Name and see Weather Forecast for upcoming 5
+        days!{" "}
+      </p>
+    </div>
+  );
+};
 
-export default About; 
+export default React.memo(About);
--- src/Components/Pages/ThankYou.js
+++ src/Components/Pages/ThankYou.js
@@ -2,10 +2,11 @@ import React from "react";
 import './ThankYou.css'
 
 const ThankYou = () => {
+    console.log('Thx')
     return (<div className='submited'>
         <h2>Thank's for reaching out</h2> 
         <p>Someone will contact you as soon as it possible!</p>
     </div>)
 }
 
-export default ThankYou; 
+export default React.memo(ThankYou); 
